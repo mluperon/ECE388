b670cf11f98085c4806c5bda0c261fe0aee95f02
dtocci1 <43154755+dtocci1@users.noreply.github.com>
2019-12-02 16:09:20 -0500

    Added Accelerometer Project
    Will test if we can get accel to work

---
+++ Accelerometer Test/Accelerometer Test/main.c
@@ -0,0 +1,21 @@
+/*
+ * Accelerometer Test.c
+ *
+ * Created: 12/2/2019 4:07:55 PM
+ * Author : dylma
+ */ 
+
+#include <avr/io.h>
+
+
+// This code will be used to test the accelerometer.
+// It should be able to poll and read data from the accelerometer using either I2C or SPI
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
